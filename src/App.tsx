5dcba0300b8a3cada32dbb17c118e69e33c3f0b0
Faith Morante <faith.morante@gmail.com>
2024-11-19 22:02:32 -0800

    Try to add a Vue Nav

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import "./App.css";
 const router = createBrowserRouter([
   {
     path: "/react18",
-    element: <div>Hello world I'm in React 18 app</div>,
+    element: (
+      <div>
+        Hello world I'm in React 18 app{" "}
+        <div>
+          <a href="/">Go Home</a>
+        </div>
+      </div>
+    ),
   },
 ]);
 
